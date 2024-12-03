a3b6592ca34db343b82d230e0e407fe9955e1556
Nils Bark <nilsebark@gmail.com>
2024-12-03 18:44:34 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(PartOne());
+            Console.WriteLine(PartTwo());
+            Console.ReadLine();
+        }
+
+        private static int PartTwo()
+        {
+            int total = 0;
+            Dictionary<int, int> occurances = new();
+            List<int> listLeft = new();
+            List<int> listRight = new();
+
+            var input = File.ReadAllLines("input.txt");
+            foreach (var line in input)
+            {
+                var splitLine = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+                listLeft.Add(splitLine[0]);
+                listRight.Add(splitLine[1]);
+            }
+
+            foreach (var item in listRight)
+            {
+                if (occurances.ContainsKey(item))
+                {
+                    occurances[item]++;
+                }
+                else
+                {
+                    occurances.Add(item, 1);
+                }
+            }
+
+            foreach (var item in listLeft)
+            {
+                if (!occurances.ContainsKey(item)) continue;
+
+                total += occurances[item] * item;
+            }
+
+            return total;
+        }
+
+        static int PartOne()
+        {
+            int total = 0;
+            List<int> listLeft = new();
+            List<int> listRight = new();
+
+            var input = File.ReadAllLines("input.txt");
+            foreach (var line in input)
+            {
+                var splitLine = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+                listLeft.Add(splitLine[0]);
+                listRight.Add(splitLine[1]);
+            }
+
+            listLeft.Sort();
+            listRight.Sort();
+
+            for (int i = 0; i < listLeft.Count; i++)
+            {
+                total += Math.Abs(listLeft[i] - listRight[i]);
+            }
+
+            return total;
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var total = 0;
+            var input = File.ReadAllLines("input.txt");
+            foreach (var line in input)
+            {
+                if (IsSafe(line))
+                {
+                    total++;
+                }
+            }
+
+            Console.WriteLine(total);
+            total = 0;
+
+            foreach (var line in input)
+            {
+                if (IsSafeEnough(line))
+                {
+                    total++;
+                }
+            }
+            Console.WriteLine(total);
+        }
+
+        static bool IsSafe(string report)
+        {
+            bool hasIncreased = false;
+            bool hasDecreased = false;
+
+            int[] splitReport = report.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            for (int i = 0; i < splitReport.Length - 1; i++)
+            {
+                int difference = splitReport[i + 1] - splitReport[i];
+
+                if (Math.Abs(difference) > 3 || Math.Abs(difference) == 0) return false;
+                if (difference > 0) hasIncreased = true;
+                if (difference < 0) hasDecreased = true;
+
+                if (hasIncreased && hasDecreased) return false;
+            }
+            return true;
+        }
+
+        static bool IsSafe(List<int> report)
+        {
+            bool hasIncreased = false;
+            bool hasDecreased = false;
+
+
+            for (int i = 0; i < report.Count - 1; i++)
+            {
+                int difference = report[i + 1] - report[i];
+
+                if (Math.Abs(difference) > 3 || Math.Abs(difference) == 0) return false;
+                if (difference > 0) hasIncreased = true;
+                if (difference < 0) hasDecreased = true;
+
+                if (hasIncreased && hasDecreased) return false;
+            }
+            return true;
+        }
+
+        static bool IsSafeEnough(string report)
+        {
+            uint dangerLevel = 0;
+            bool hasIncreased = false;
+            bool hasDecreased = false;
+
+            List<int> splitReport = report.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            if (IsSafe(splitReport)) return true;
+
+            for (int i = 0; i < splitReport.Count; i++)
+            {
+                List<int> tempReport = new();
+                tempReport = tempReport.Concat(splitReport).ToList();
+                tempReport.RemoveAt(i);
+                if (IsSafe(tempReport)) return true;
+            }
+
+            return false;
+        }
+    }
+}
